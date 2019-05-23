9fba6aa62995adae916db8960a529020d1b2bf93
Bryam Barrera <bryan19972011@hotmail.es>
2019-05-23 12:08:31 -0500

    Formulario-js-css-php

---
+++ js/validaciones_usuario.js
@@ -0,0 +1,114 @@
+function validarCamposObligatorios(){
+
+    var bandera = false;
+    for (var i = 0; i<document.forms[0].length; i++){
+        var elemento = document.forms[0].elements[i];
+        if (elemento.value.trim() == ''){
+            bandera = true;
+            elemento.classList.add('error');
+            break;
+        }
+    }
+    if (bandera == true){
+
+        alert('Llenar todos los campos');
+        document.getElementById('p').classList.add('p');
+        return false;
+
+    } else {
+        var fecha = validarFecha();
+        var cedula = validarCedula();
+        var correo = validarCorreo();
+        if (fecha || cedula || correo) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+}
+
+function wErrores(elemento) {
+    elemento.classList.remove('error');
+    document.getElementById('p').classList.remove('p');
+}
+
+function validarNumero(string){
+    var out = '';
+    var filtro = '1234567890';
+    
+    for (var i = 0; i < string.length; i++)
+        if (filtro.indexOf(string.charAt(i)) != -1) 
+        out += string.charAt(i);
+    return out;
+} 
+
+function validarCedula() {
+    var cedula = document.getElementById('ced').value.trim();
+    if (cedula.substring(0, 2) > 10) {
+        document.getElementById('ced').classList.add('error');
+        document.getElementById('cd').classList.add('p');
+        alert('Cédula no válida');
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validarLetras(string){
+    var out = '';
+    var filtro = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ ';
+        for (var i = 0; i < string.length; i++)
+        if (filtro.indexOf(string.charAt(i)) != -1) 
+        out += string.charAt(i);
+    return out;
+} 
+
+function dosPalabras(string) {
+    var out = '';
+    var array = string.split(' ');
+    if (array.length == 1){
+        out += array[0];
+    } else {
+        out += array[0] + ' ' + array[1];
+    }
+    
+    return out;
+}
+
+function validarFecha() {
+    var array = document.getElementById('fec').value.split('/');
+    var fecha = new Date(array[2], array[1], array[0]);
+    if (array.length == 3 && fecha 
+        && array[0] == fecha.getDate() 
+        && array[1] == fecha.getMonth() 
+        && array[2] == fecha.getFullYear()) {
+        return false;
+    } else {
+        document.getElementById('fec').classList.add('error');
+        document.getElementById('f').classList.add('p');
+        alert('Fecha mal ingresada, usar formato dd/mm/yyyy');
+        return true;
+    }
+}
+
+
+function validarCorreo() {
+    var array = document.getElementById('ema').value.split('@');
+
+    if(array[0].length < 3) {
+        document.getElementById('ema').classList.add('error');
+        alert('Correo no válido')
+        return true;
+    } else {
+        if (!(array[1] == 'ups.edu.ec') && !(array[1] == 'est.ups.edu.ec')) {
+            document.getElementById('ema').classList.add('error');
+            document.getElementById('e').classList.add('p');
+            alert('Extensión no válida')
+            return true;
+        } else {
+            return false;
+        }
+        
+    } 
+}
