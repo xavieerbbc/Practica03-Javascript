2331876e47f0bebd9e6bac7423c6dd356ec17077
Bryam Barrera <bryan19972011@hotmail.es>
2019-05-23 12:14:41 -0500

    correccion de errores

--- FORMULARIO/js/validaciones.php
+++ FORMULARIO/js/validaciones.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html>
-
 	<head>
 		<meta charset="utf-8">
-		<title>Crear Nuevo Usuario</title>
+		<title>Nuevo Usuario</title>
 		<link rel="stylesheet" type="text/css" href="../css/style.css">
 	</head>
-
 	<body>
 		<?php
 			echo "<h1>Bienvenido, pasaste las validaciones!</h1>";
 		?>
 	</body>
-
 </html>
